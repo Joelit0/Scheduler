8f6d2d3f649849cdb80b1112f1d9aaabd5157aae
Seba-Quintana <SEBASTIAN.QUINTANA@correo.ucu.edu.uy>
2022-11-14 23:27:56 -0300

    scheduler xd

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,18 @@
 public class Main {
   public static void main(String[] args) {
-    Process process = new Process("Process 1", 7000, 6000);
-    process.start();
-
-    while (process.isAlive()) {}
-
-    process.start();
+    Scheduler scheduler = new Scheduler();
+    Process process1 = new Process("Process 1", scheduler);
+/*    Process process2 = new Process("Process 2", scheduler);
+    Process process3 = new Process("Process 3", scheduler);
+    Process process4 = new Process("Process 4", scheduler);
+    Process process5 = new Process("Process 5", scheduler);
+    scheduler.start();
+    process1.start();
+    process2.start();
+    process3.start();
+    process4.start();
+    process5.start();*/
+    scheduler.start();
+    process1.start();
   }
 }
--- src/main/java/Process.java
+++ src/main/java/Process.java
@@ -4,73 +4,76 @@ import javax.swing.Timer;
 import java.lang.Thread;
 
 public class Process extends Thread {
-  private enum states {
-    READY,
-    IN_PROGRESS,
-    BLOCKED,
-    SUSPENDED,
-    DONE
-  }
-
-  private String name;
-  private states state;
-  private Timer durationTimer;
-  private Timer schedulerTimer;
-
-  private boolean x = false;
-
-  public Process(String name, int durationTime, int schedulerTimeout) {
-    this.name = name;
-    this.state = states.READY;
-
-    this.durationTimer = new Timer(durationTime, new ActionListener()  {
-      public void actionPerformed(ActionEvent e)
-      {
-        finish();
-      }
-    });
-
-    this.schedulerTimer = new Timer(schedulerTimeout, new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        block();
-      }
-    });
-
-    this.durationTimer.setRepeats(false);
-    this.schedulerTimer.setRepeats(false);
-  }
-
-  public String getEstado() {
-    return this.state.toString();
-  }
-
-  @Override
-  public void run() {
-    if (this.state == states.READY) {
-      System.out.println("Dando CPU al proceso " + this.name);
-
-      this.durationTimer.start();
-      this.schedulerTimer.start();
-
-      System.out.println(x);
-
-      while (true) { }
+    private enum states {
+        READY,
+        IN_PROGRESS,
+        BLOCKED,
+        SUSPENDED,
+        DONE
     }
-  }
 
-  public void block() {
-    this.durationTimer.stop();
-    System.out.println("Bloqueando");
-    this.state = states.BLOCKED;
-    this.x = true;
-    this.state = states.READY;
-    this.interrupt();
-  }
+    private String processName;
+    private states state;
+    private Scheduler scheduler;
 
-  public void finish() {
-    System.out.println("Terminando");
-    this.schedulerTimer.stop();
-    this.state = states.DONE;
-    this.interrupt();
-  }
+
+    public String getProcessName() {
+        return processName;
+    }
+
+    public Process(String name, Scheduler scheduler){
+        this.processName = name;
+        this.scheduler = scheduler;
+    }
+
+
+    public String getEstado() {
+        return this.state.toString();
+    }
+
+    public void setEstadoIN_PROGRESS() {
+        this.state = states.IN_PROGRESS;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("initializing process thread...");
+        System.out.println("process ready to execute");
+        this.state = states.READY;
+        scheduler.Listo(this);
+        while (state != states.IN_PROGRESS){
+            try {
+                synchronized (Syncro.syncro)
+                {
+                    Syncro.syncro.wait();
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        try {
+            System.out.println(Thread.currentThread().getName());
+            Work();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void Work() throws InterruptedException {
+        System.out.println("working...");
+        sleep(1000000000);
+        finish();
+    }
+
+    public void block() {
+        System.out.println("Bloqueando");
+        this.state = states.BLOCKED;
+        this.interrupt();
+    }
+
+    public void finish() {
+        System.out.println("done");
+        this.state = states.DONE;
+        this.interrupt();
+    }
 }
